13d3900392fe7336f2ecfa102bfbd92027283779
Akai Kaede <genko2@hotmail.com>
2026-05-27 08:26:56 -0700

    Implement dependency injection and configure services for media capture and view models

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,41 +1,40 @@
-﻿using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using IntVue.Services;
+using IntVue.ViewModels;
 
 namespace IntVue;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
 public partial class App : Application
 {
+    public static IServiceProvider Services { get; private set; } = null!;
+
+    // Backing field for the main application window
     private Window? _window;
 
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
     public App()
     {
         InitializeComponent();
+        Services = ConfigureServices();
     }
 
-    /// <summary>
-    /// Invoked when the application is launched.
-    /// </summary>
-    /// <param name="args">Details about the launch request and process.</param>
+    // InitializeComponent is provided by generated XAML code (App.g.i.cs)
+
+    private static IServiceProvider ConfigureServices()
+    {
+        var services = new ServiceCollection();
+
+        // Services
+        services.AddSingleton<IMediaCaptureService, MediaCaptureService>();
+        // navigation, data services, etc:
+        // services.AddSingleton<INavigationService, NavigationService>();
+
+        // ViewModels
+        services.AddTransient<MainViewModel>();
+
+        return services.BuildServiceProvider();
+    }
+
     protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
     {
         _window = new MainWindow();
